11aa5492ae641c1d9bd661c08dbbbadca78dfa06
amcbarnett <mckingdom@Ancils-MacBook-Pro.local>
2018-04-17 19:23:19 -0400

    test credential

--- GCP/provider.tf
+++ GCP/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  #credentials = "${var.gcp_credentials}"
-  credentials = "${base64decode(google_service_account_key.acceptance.private_key)}"
+  credentials = "${var.gcp_credentials}"
+  #credentials = "${base64decode(google_service_account_key.acceptance.private_key)}"
   project     = "${var.gcp_project}"
   region      = "${var.gcp_region}"
 }
