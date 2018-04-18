6b3a2b580cd6bcd42df52fb18fcbd022f1648cd1
amcbarnett <mckingdom@Ancils-MacBook-Pro.local>
2018-04-17 20:40:16 -0400

    Fix Service Name

--- GCP/instance.tf
+++ GCP/instance.tf
@@ -20,7 +20,7 @@ resource "google_service_account_iam_binding" "cb-computeuser-iam" {
 }
 
 resource "google_service_account_iam_binding" "cb-instanceadmin-iam" {
-  service_account_id = "${google_service_account.cloudbreak-gcp-service.name"
+  service_account_id = "${google_service_account.cloudbreak-gcp-service.name}"
   role        = "roles/compute.instanceAdmin.v1"
 
   members = [
